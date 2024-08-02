325de3b28ea32bec4243eb66ecd4a4639e6e009b
hohserg1 <hohserg1@yandex.ru>
2024-08-02 15:56:37 +0300

    Update PacketStartPathRecording.java

--- src/main/java/hunternif/mc/atlas/network/server/PacketStartPathRecording.java
+++ src/main/java/hunternif/mc/atlas/network/server/PacketStartPathRecording.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.relauncher.Side;
 
 import java.io.IOException;
@@ -14,13 +15,15 @@ import java.io.IOException;
 public class PacketStartPathRecording extends AbstractMessage.AbstractServerMessage<PacketStartPathRecording> {
     private EnumHand hand;
     private long activeKey;
+    private BlockPos start;
 
     public PacketStartPathRecording() {
     }
 
-    public PacketStartPathRecording(EnumHand hand, long activeKey) {
+    public PacketStartPathRecording(EnumHand hand, long activeKey, BlockPos start) {
         this.hand = hand;
         this.activeKey = activeKey;
+        this.start = start;
     }
 
     @Override
@@ -28,19 +31,21 @@ public class PacketStartPathRecording extends AbstractMessage.AbstractServerMess
         int i = buffer.readByte();
         hand = 0 <= i && i < EnumHand.values().length ? EnumHand.values()[i] : EnumHand.MAIN_HAND;
         activeKey = buffer.readLong();
+        start = BlockPos.fromLong(buffer.readLong());
     }
 
     @Override
     protected void write(PacketBuffer buffer) throws IOException {
         buffer.writeByte(hand.ordinal());
         buffer.writeLong(activeKey);
+        buffer.writeLong(start.toLong());
     }
 
     @Override
     protected void process(EntityPlayer player, Side side) {
         ItemStack heldItem = player.getHeldItem(hand);
         if (heldItem.getItem() == RegistrarAntiqueAtlas.ARIADNE_THREAD) {
-            ItemAriadneThread.activate(heldItem, activeKey);
+            ItemAriadneThread.activate(heldItem, activeKey, start);
         }
     }
 }
